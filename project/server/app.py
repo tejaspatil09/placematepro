3285965c83a3752f41332ef9e1ab1bc1af191918
wavekarchinmay <59259568+wavekarchinmay@users.noreply.github.com>
2024-02-08 01:10:03 +0530

    Changes Done
    - Fixed minor bugs
    - Basic API functionality done
    - Result page added
    - Project directory structure changed

---
+++ project/server/app.py
@@ -0,0 +1,85 @@
+import json
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import mysql.connector
+
+# pip install mysql-connector-python-rf
+# pip install flask_cors
+
+# Mysql config
+app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
+# For Flask 2.3 and later use this:
+app.json.sort_keys = False
+CORS(app)
+
+# 404 page
+@app.errorhandler (404)
+def not_found (error=None):
+    response = {
+        'message': 'Invalid request',
+    }
+    return jsonify(response)
+
+@app.route('/post', methods=['GET'])
+@app.route('/result', methods=['GET'])
+
+def get_result():
+    try:
+        conn = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="plecematepro"
+        )
+        
+        filter = request.args.get('filter')
+
+        colsStr = 'name,class,academic_performance,technical_skills,core_knowledge,puzzle_solving,programming_skills,project,internship,backlog,coding_skills,aptitude_skills,communication_skills,presentation_skills,english_proficiency,management_skills,result';
+        if(filter != "None"):
+            colsStr = filter;
+        
+        cur = conn.cursor()
+        cur.execute('SELECT '+colsStr+' FROM data')
+        result = cur.fetchall()
+        alldata = [];
+
+        cols = colsStr.split(',')
+        
+        data = {};
+
+        try:
+            i = 0
+            for row in result:
+                tmpdata = {}
+
+                for j in range(0, len(row)):
+                    tmpdata[cols[j]] = row[j];
+
+                data[i] = tmpdata
+                
+                i+=1
+        except Exception as e:
+            print(e)
+
+        response = {
+            'app' : 'plecematepro',
+            'request': request.remote_addr,
+            'api_version' : 1.0,
+            'status' : 200,
+            'data' : {
+                'cols' : cols,
+                'record' : data
+            }
+        }
+        
+        return jsonify(response), 200
+    except Exception as e:
+        print(e)
+    finally:
+        cur.close()
+
+        
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8081)
+
---
+++ project/server/db/data.sql
@@ -0,0 +1,57 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Feb 07, 2024 at 07:37 PM
+-- Server version: 5.7.26
+-- PHP Version: 7.2.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `plecematepro`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data`
+--
+
+DROP TABLE IF EXISTS `data`;
+CREATE TABLE IF NOT EXISTS `data` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(40) NOT NULL,
+  `class` varchar(20) NOT NULL,
+  `academic_performance` int(11) NOT NULL,
+  `technical_skills` int(11) NOT NULL,
+  `core_knowledge` int(11) NOT NULL,
+  `puzzle_solving` int(11) NOT NULL,
+  `programming_skills` int(11) NOT NULL,
+  `project` int(11) NOT NULL,
+  `internship` int(11) NOT NULL,
+  `backlog` int(11) NOT NULL,
+  `coding_skills` int(11) NOT NULL,
+  `aptitude_skills` int(11) NOT NULL,
+  `communication_skills` int(11) NOT NULL,
+  `presentation_skills` int(11) NOT NULL,
+  `english_proficiency` int(11) NOT NULL,
+  `management_skills` int(11) NOT NULL,
+  `result` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
